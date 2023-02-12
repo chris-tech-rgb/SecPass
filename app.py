21a010419da7b9c95685aef385d8a69cab3f9b8b
Chris <turcottezenobia@gmail.com>
2023-02-12 11:53:32 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from dotenv import load_dotenv
+from flask import Flask
+from flask import render_template
+
+load_dotenv('./.flaskenv')
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
